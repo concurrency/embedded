fdec197f3bd1070fc462dbba4b7d01c5474f7bf4
Matt Jadud <matt@jadud.com>
2015-11-28 18:46:03 -0500

    Seems to build a library. :

--- runtime/t2-table.bash
+++ runtime/t2-table.bash
@@ -5,12 +5,8 @@ python make-dispatch.py \
 	ins_barrier.c \
 	ins_chan.c \
 	ins_float.c \
-	ins_pi.c \
-	ins_mobile.c \
 	ins_pri.c \
 	ins_proc.c \
-	ins_rmox.c \
 	ins_sec.c \
 	ins_t800.c \
-	ins_t9000.c \
 	ins_timer.c
