57fd0f648e695f750a0355ae04833b63036d7a25
Matt Jadud <matt@jadud.com>
2018-07-10 11:02:24 -0400

    Hard-coding config.h.
    As I re-learn things about the infrastructure, thinking through how to automate this with the kroc tree will matter.
    
    For now, I'll settle for getting things to build.

--- compiler/config.h
+++ compiler/config.h
@@ -1,43 +1,164 @@
-#ifndef COMPILER_CONFIG_H_SEEN
-#define COMPILER_CONFIG_H_SEEN
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
 
-/* Set if compiling ilibr & tcofflib under a UNIX. */
-#define TARGET_OS_IS_UNIX 1
-/* Set if compiling ilibr & tcofflib with GCC. */
-#define COMPILER_IS_GCC 1
-/* Set if compiling for an x86 platform. */
-#define TARGET_CPU_IS_I386 1
-/* Unconditional */
-#define OC 1
-/* Unconditional */
-#define OCCAM2_5 1
-/* Target CPU */
-#define TARGET_CANONICAL "i386"
-/* Define to target KROC ETC. */
-#define CODE_GEN_KROC_ASM 1
-/* To enable user-defined operators. */
-#define USER_DEFINED_OPERATORS 1
-/* Default enabled. */
-#define MOBILES 1
-/* Default enabled. */
-#define PROCESS_PRIORITY 1
-/* Depends on compiler... GCC OK? */
+/* define if back-end debugging required */
+/* #undef BACKENDTRACE */
+
+/* define if can inline */
 #define CAN_USE_INLINE 1
-/* Default enabled. */
-#define INITIAL_DECL 1
-/* This has not changed for 10 years... why start now? */
-#define VERSION "1.4.0"
 
-/* The size of `signed int', as computed by sizeof. */
-#define SIZEOF_SIGNED_INT 4
+/* define to target KRoC ETC */
+#define CODE_GEN_KROC_ASM 1
 
-/* Define to 1 if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H 1
+/* define if using gcc */
+/* #undef COMPILER_IS_GCC */
+
+/* define to enable debugging */
+/* #undef DEBUG */
+
+/* define for next-symb debugging */
+/* #undef DEBUG_NEXTSYMB */
+
+/* Define to 1 if you don't have `vprintf' but do have `_doprnt.' */
+/* #undef HAVE_DOPRNT */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <limits.h> header file. */
+#define HAVE_LIMITS_H 1
+
+/* Define to 1 if you have the <malloc.h> header file. */
+/* #define HAVE_MALLOC_H 0 */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
 
 /* Define to 1 if you have the <stdarg.h> header file. */
 #define HAVE_STDARG_H 1
 
-/* Define to 1 if you have the <malloc.h> header file. */
-/* #undef HAVE_MALLOC_H */
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
 
-#endif /* COMPILER_CONFIG_H_SEEN */
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strrchr' function. */
+#define HAVE_STRRCHR 1
+
+/* Define to 1 if you have the `strspn' function. */
+#define HAVE_STRSPN 1
+
+/* Define to 1 if you have the `strstr' function. */
+#define HAVE_STRSTR 1
+
+/* Define to 1 if you have the `strtol' function. */
+#define HAVE_STRTOL 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the `vprintf' function. */
+#define HAVE_VPRINTF 0
+
+/* define if the host OS is UNIX */
+#define HOST_OS_IS_UNIX 1
+
+/* define to support INITIAL decls */
+#define INITIAL_DECL 1
+
+/* define to support MOBILEs */
+#define MOBILES 0
+
+/* define if quad alignment needed */
+/* #undef NEED_QUAD_ALIGNMENT */
+
+/* define to build occam compiler */
+#define OC 1
+
+/* define to include occam 2.1 support */
+#define OCCAM2_5 1
+
+/* Name of package */
+#define PACKAGE "occ21"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "kroc-bugs@kent.ac.uk"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "occ21"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "occ21 1.4.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "occ21"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.4.1"
+
+/* define if process-priority desired */
+#define PROCESS_PRIORITY 1
+
+/* The size of `signed int', as computed by sizeof. */
+#define SIZEOF_SIGNED_INT 2
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* define if memory accesses must be aligned */
+/* #undef TARGET_ACCESSALIGNED */
+
+/* define target alias */
+#define TARGET_ALIAS "avr"
+
+/* define target */
+#define TARGET_CANONICAL "avr-unknown-none"
+
+/* define target CPU */
+#define TARGET_CPU "avr"
+
+/* define if i386 processor family */
+/* #undef TARGET_CPU_IS_I386 */
+
+/* define if generating code for a big-endian machine */
+/* #undef TARGET_IS_BIGENDIAN */
+
+/* define target OS */
+#define TARGET_OS "none"
+
+/* define target vendor */
+#define TARGET_VENDOR "unknown"
+
+/* define to enable user-defined-operators */
+#define USER_DEFINED_OPERATORS 1
+
+/* Version number of package */
+#define VERSION "1.4.1"
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
